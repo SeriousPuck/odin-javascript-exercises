a575c54cc76e5db2aace6209010f580d528592df
Joey Waner <124863038+SeriousPuck@users.noreply.github.com>
2026-06-08 16:57:23 -0700

    Complete 07_reverseString committed
    completed the 07_reverseString code as requested in the README

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(originalString) {
+    let reversedString = "";
+    let lengthString = originalString.length - 1;
+    for (let i = 0; i <= lengthString; i++){
+        reversedString =  reversedString + originalString.charAt(lengthString - i);
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
