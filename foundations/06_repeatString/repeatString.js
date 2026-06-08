b074263b4ae78b76ef575d0d9e81758358c42394
Joey Waner <124863038+SeriousPuck@users.noreply.github.com>
2026-06-08 16:33:49 -0700

    Complete the code for 06_repeateString
    completed the code for 06_repeateString as required in the README
    made sure it passed all unit tests

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num === -1){
+        return 'ERROR'
+    } else {
+        const arrayWords = [];
+        for (let i = 0; i < num; i++){
+            arrayWords[i] = str;
+        }
+        return arrayWords.join("")
+    }
 };
 
 // Do not edit below this line
